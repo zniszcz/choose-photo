12ca8ef609eb73c74d21a544da7dac34c95352d2
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-07-17 02:20:32 +0200

    Add search route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <main-menu></main-menu>
-    <router-outlet></router-outlet>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
   `,
   styles: [`
 
