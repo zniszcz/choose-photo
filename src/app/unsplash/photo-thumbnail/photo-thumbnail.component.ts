052c304e32f68e8f3884d1f0c6081bda93a63a6d
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-07-22 06:00:36 +0200

    Add thumbnail template

--- src/app/unsplash/photo-thumbnail/photo-thumbnail.component.ts
+++ src/app/unsplash/photo-thumbnail/photo-thumbnail.component.ts
@@ -3,14 +3,17 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'photo-thumbnail',
   templateUrl: './photo-thumbnail.component.html',
-  styles: ['./photo-thumbnail.component.scss']
+  styleUrls: ['./photo-thumbnail.component.scss']
 })
 export class PhotoThumbnailComponent implements OnInit {
 
   @Input()
   photo;
 
+  background = '';
+
   constructor() { }
+
 
   ngOnInit() {
   }
